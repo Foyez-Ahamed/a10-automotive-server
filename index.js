cbf185c4482b091489cc0754ec148f499e3c9776
Foyez-Ahamed <foyez1435@gmail.com>
2023-10-19 22:27:05 +0600

    find single  prodcut id for show details product

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ async function run() {
 
     const brandsCategoryCollection = client.db('brandsCategoriesDB').collection('brandsCategory');
 
-    
+
     // crud operation for brands // 
     app.get('/brands', async(req, res) => {
       const cursor = brandsCollections.find();
@@ -66,6 +66,13 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
 
+    })
+
+    app.get('/product/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = { _id : new ObjectId(id) }
+      const result = await brandsCategoryCollection.findOne(query);
+      res.send(result);
     })
 
 
